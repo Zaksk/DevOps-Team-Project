4bda7592302631a95ca999dedaf19e64a2bc0066
QAEdd <edd-alexander@hotmail.com>
2022-04-27 08:59:44 +0000

    jenkins changes

--- Jenkins/build.sh
+++ Jenkins/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose build --parallel
-docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}
+sudo docker-compose build --parallel
+sudo docker login -u $DOCKER_CREDS_USR -p $DOCKER_CREDS_PSW
 
