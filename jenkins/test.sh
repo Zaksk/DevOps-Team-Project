5d48a8feca47ce39bf8a0d64c5b8bf3c7bc15428
QAEdd <edd-alexander@hotmail.com>
2022-04-28 09:15:14 +0000

    Sudo'd

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-apt update && sudo apt install openjdk-11-jdk -y
-apt install curl -y
-install node -y
+sudo apt update && sudo apt install openjdk-11-jdk -y
+sudo apt install curl -y
+sudo install node -y
 
-curl -sL https://deb.nodesource.com/setip_16.x -o nodesource_setup.sh
-bash nodesource_setup.sh
-apt install nodejs -y
+sudo curl -sL https://deb.nodesource.com/setip_16.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb -y
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
-npm i
-npm run test-headless
+sudo npm i
+sudo npm run test-headless
 
-./mvnw clean test
+sudo ./mvnw clean test
