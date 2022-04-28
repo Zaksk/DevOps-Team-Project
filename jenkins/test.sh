f9c3e15223ccb17df354abe9e2d41a3d310d60ab
Jack Wearn <wearnjack@gmail.com>
2022-04-28 09:32:16 +0100

    Added test scrpt

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+sudo apt update && sudo apt install openjdk-11-jdk -y
+sudo apt install curl -y
+nvm install node -y
+
+curl -sL https://deb.nodesource.com/setip_16.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+npm i
+npm run test-headless
+
+./mvnw clean test
