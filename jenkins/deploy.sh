3dd5626b4815f96a51776db97a52e9699bb450c2
QAEdd <edd-alexander@hotmail.com>
2022-04-28 14:56:36 +0000

    .

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo kubectl apply -f K8/backend.yaml --context emoluster
-sudo kubectl apply -f K8/frontend.yaml --context emoluster
+aws eks --region eu-west-2 update-kubeconfig --name emoluster
+kubectl apply -f K8/backend.yaml
+kubectl apply -f K8/frontend.yaml
