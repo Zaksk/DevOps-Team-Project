cbde21105c844f7daa9f00a54cd043953b1f2cf8
QAEdd <edd-alexander@hotmail.com>
2022-04-28 14:52:43 +0000

    emoluster

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo kubectl apply -f backend.yaml
-sudo kubectl apply -f frontend.yaml
+sudo kubectl apply -f K8/backend.yaml --context emoluster
+sudo kubectl apply -f K8/frontend.yaml --context emoluster
