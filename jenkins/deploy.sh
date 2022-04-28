ede9b8d0a0ea49f44c6f01045f69dbb25233c0f8
QAEdd <edd-alexander@hotmail.com>
2022-04-28 08:50:18 +0000

    updated jenkins file

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-kubectl apply -f nginx.yaml
 kubectl apply -f backend.yaml
 kubectl apply -f frontend.yaml
--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo apt update && sudo apt install openjdk-11-jdk -y
-sudo apt install curl -y
-nvm install node -y
+apt update && sudo apt install openjdk-11-jdk -y
+apt install curl -y
+install node -y
 
 curl -sL https://deb.nodesource.com/setip_16.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs -y
+bash nodesource_setup.sh
+apt install nodejs -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+apt install ./google-chrome-stable_current_amd64.deb -y
 
 npm i
 npm run test-headless
